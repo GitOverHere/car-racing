7e8878f9a9c2eb4aecb456645291bf27134abd18
GitOverHere <sahu_binayak@yahoo.com>
2021-07-29 14:35:50 -0700

    Here it is! I ran into some issues down the road for commiting, but I am now able to bring this game to you.

---
+++ Desktop/Video_Games_I_Made/Racing game/Assets/Scripts/Car.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Car: MonoBehaviour {
+
+  public WheelCollider frontLeftWheelCollider, frontRightWheelCollider, backLeftWheelCollider, backRightWheelCollider;
+  public Transform frontleft, frontright, backleft, backright;
+  public float maxSteeringAngle = 75.0f;
+  public float motorTorque = 10000.0f;
+  public float brakeTorque = 50f;
+  public bool Reverse = false;
+ 
+
+  // Use this for initialization
+  void Start() {
+
+  }
+  
+  
+  private void Steer(){
+	  
+	   float h = Input.GetAxis("Horizontal");
+
+	  frontLeftWheelCollider.steerAngle = maxSteeringAngle*h;
+      frontRightWheelCollider.steerAngle = maxSteeringAngle*h;
+      frontLeftWheelCollider.transform.Rotate(0,maxSteeringAngle*h, 0);
+      frontRightWheelCollider.transform.Rotate(0,maxSteeringAngle*h,0);
+	  
+  }
+
+  void Update() {
+
+   
+    float v = Input.GetAxis("Vertical");
+	Steer();
+    if(Input.GetKey("r")){
+		if(!Reverse){
+			Reverse= true;
+		}
+		else {
+			Reverse = false;
+		}
+		
+	}
+
+    if (v > 0f) {
+
+      if (frontLeftWheelCollider.motorTorque <= 0f) {
+        frontLeftWheelCollider.motorTorque += motorTorque;
+        frontRightWheelCollider.motorTorque += motorTorque;
+      } else 
+	  {
+		  if(frontLeftWheelCollider.motorTorque < 100000f){
+        frontLeftWheelCollider.motorTorque *= motorTorque;
+        frontRightWheelCollider.motorTorque *= motorTorque;
+		  }
+      }
+
+      frontLeftWheelCollider.brakeTorque = 0f;
+	  frontRightWheelCollider.brakeTorque = 0f;
+    }
+
+    if (v < 0f) {
+		if(!Reverse){
+      if (frontLeftWheelCollider.brakeTorque > 0f) {
+        frontLeftWheelCollider.brakeTorque += frontLeftWheelCollider.brakeTorque;
+        frontRightWheelCollider.brakeTorque += frontRightWheelCollider.brakeTorque;
+      } else {
+        frontLeftWheelCollider.brakeTorque += brakeTorque;
+        frontRightWheelCollider.brakeTorque += brakeTorque;
+      }
+		}
+
+      frontLeftWheelCollider.motorTorque -= 2 * frontLeftWheelCollider.motorTorque;
+      frontRightWheelCollider.motorTorque -= 2 * frontRightWheelCollider.motorTorque;
+    }
+
+    frontleft.transform.Rotate(0, 0, (frontLeftWheelCollider.rpm / 60) / Time.deltaTime, Space.Self);
+    frontright.transform.Rotate(0, 0, (frontRightWheelCollider.rpm / 60) / Time.deltaTime, Space.Self);
+    backleft.transform.Rotate(0, 0, (frontLeftWheelCollider.rpm / 60) / Time.deltaTime, Space.Self);
+    backright.transform.Rotate(0, 0, (frontRightWheelCollider.rpm / 60) / Time.deltaTime, Space.Self);
+  }
+}
---
+++ Desktop/Video_Games_I_Made/Racing game/Assets/Scripts/Hovercar.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Hovercar : MonoBehaviour
+{
+    public GameObject car;
+    public AudioSource source;
+    public AudioClip rev;
+    public Image speedmeter, knob;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
